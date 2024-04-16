63d6cc5eeaf038f6800fc00f343c7c5e28422fd3
João Gustavo <joao@exemplo.com>
2024-04-16 14:57:07 -0300

    alterações

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ $(document).ready(function(){
     $('#input').change(function(){
         var input = $(this).val();
         $('ul').append('<li>' + input + ' <i class="fas fa-check"></i> <i class="fas fa-trash"></i> </li>');
-        $(this).val();
+        $(this).val('');
     });
     $('ul').on('click','.fa-trash',function(){
         $(this).parent('li').fadeOut(200);
     });
 
     $('ul').on('click','.fa-check',function(){
-        $(this).parent('li').toggleClass(200);
+        $(this).parent('li').toggleClass('checked');
     });
 });
